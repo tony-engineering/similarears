f3698034b552a040a42aa87a66a550e32a2ca1ea
Antoine <rougeot.antoine@gmail.com>
2017-01-02 16:42:49 +0100

    First work

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var resolve = require('./routes/resolve');
+var likes = require('./routes/likes');
 
 var app = express();
 
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', resolve);
+app.use('/likes', likes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
